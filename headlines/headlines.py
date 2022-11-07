33abd647a4ec3e1259b69b5d34453c6a9c86d594
unknown <sm.tasmimulhuda@gmail.com>
2022-11-08 00:43:30 +0600

    ch3 template

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import feedparser
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -10,29 +11,21 @@ RSS_FEEDS = {
     'fox': 'http://feeds.foxnews.com/foxnews/latest',
     'iol': 'http://www.iol.co.za/cmlink/1.640'
     }
+@app.route("/")
 @app.route("/<publication>")
 
+@app.route("/bbc")
+def bbc():
+    return get_news('bbc')
 
-# @app.route("/bbc")
-# def bbc():
-#     return get_news('bbc')
-
-# @app.route('/cnn')
-# def cnn():
-#     return get_news('cnn')
+@app.route('/cnn')
+def cnn():
+    return get_news('cnn')
 
 
 def get_news(publication='bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][2]
-    return  f"""<html>
-    <body>
-    <h1>{publication.upper()} Headlines</h1>'
-    <b>{first_article.get("title")}</b> <br/>
-    <i>{first_article.get("published")}</i> <br/>
-    <p>{first_article.get("summary")}</p> <br/>
-    </body>
-    </html>"""
+    return render_template("home.html", articles= feed['entries'])
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
